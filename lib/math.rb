2e038be35cfce590cb185d4ef069302383d84e0c
Leonora Squires <leonora.squires@gmail.com>
2020-04-22 19:29:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,15 +9,17 @@ dif
 end
 
 def division(num1, num2)
-
+div = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+mul = num1 * num2
+mul
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
+mod
 end
 
 def square_root(num)
